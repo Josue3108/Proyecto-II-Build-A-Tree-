5570fb68f3cd6b0f134b3f7203dcad8cecb28d3d
Jafet Dixon Solano <jafet.dixon@gmail.com>
2020-11-27 15:54:21 -0600

    Cambios árbol

--- Java/src/Trees/BSTree.java
+++ Java/src/Trees/BSTree.java
@@ -1,4 +1,45 @@
 package Trees;
 
-public class BSTree {
+public class BSTree<T> {
+
+
+    private NodeTree<T> root = null;
+
+
+    public NodeTree<T> getRoot() {
+        return root;
+    }
+
+    public T getRootData(){
+        return root.getData();
+    }
+
+    public void setRoot(NodeTree<T> root) {
+        this.root = root;
+    }
+
+    public void setRoot(T data){
+        this.root  = new NodeTree<>(data);
+    }
+
+    public void setNode(NodeTree<T> node){
+        if (this.root == null){
+            this.root = node;
+
+        }else{
+
+            NodeTree<T> subroot = this.root;
+            NodeTree<T> actual = this.root;
+
+            while (actual!=null){
+
+                if(actual)
+
+
+            }
+        }
+
+    }
+
+
 }
